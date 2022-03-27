d8848b027a495df258f68f5857d078c438ead998
Derek Pepple <dpepple@nd.edu>
2022-03-27 00:01:58 -0400

    Fred Split Up Jones

--- server.py
+++ server.py
@@ -6,31 +6,48 @@ import os
 
 PORT = 9999
 
-class ArgumentsTestHandler(tornado.web.RequestHandler):
+class homeHandler(tornado.web.RequestHandler):
+    def get(self):
+        data = get_json('fakedata.json')
+        self.render('todays_menu.html', data=data)
+        result = self.get_argument("servings", "")
+        print(result)
+
+class reccommendationsHandler(tornado.web.RequestHandler):
     def get(self):
         #name = self.get_argument('name', '')
 
         data = get_json('fakedata.json')
         self.render('today.html', data=data)     # Render template with arguments
+
+#TODO
+class submitHandler(tornado.web.RequestHandler):
+    def get(self):
+        #name = self.get_argument('name', '')
+
+        data = get_json('fakedata.json')
+        self.render('today.html', data=data)     # Render template with arguments
+
+class todayMenuHandler(tornado.web.RequestHandler):
+    def get(self):
+        data = get_json('fakedata.json')
+        self.render('todays_menu.html', data=data)
+
         
-
-# Main Execution
-
-
 def get_json(PATH):
     with open(PATH, 'r') as fh:
         data = json.load(fh)
         return data
         
-
-
 def main():
 
     settings = {
         "static_path": os.path.join(os.path.dirname(__file__), "static")
     }
     application = tornado.web.Application([
-        (r'/', ArgumentsTestHandler),
+        (r'/', homeHandler),
+        (r'/today.html',reccommendationsHandler),
+        (r'/todays_menu.html', todayMenuHandler)
     ], **settings)
     application.listen(PORT)
 
